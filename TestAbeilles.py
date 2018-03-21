141719a39056fdb30777b12b8f7d9441503d7fbe
Maxlm4 <36695417+Maxlm4@users.noreply.github.com>
2018-03-21 10:59:48 +0100

    Add files via upload

--- TestAbeilles.py
+++ TestAbeilles.py
@@ -13,23 +13,25 @@ for i in range(Nb_ruches):
     y0 = y % N              #coordonnées de la rûche
     Liste_ruches.append((x0,y0))
 Info_ruches=[] #Toutes les informations sur chaque ruche
-Nb_abeilles=70000 #nombre initial d'abeilles dans la ruche
+Nb_abeilles=30000 #nombre initial d'abeilles dans la ruche
 Nb_ab_dedans=Nb_abeilles #nombre d'abeiles dans la rûche
 Nb_but_dehors=0  #nombre de butineuses hors de la ruche
 reperage=0 #nombre de zones florales découvertes
 liste_reper=[] #liste des zones florales découvertes
 distance=0 #distance initiale du repérage par rapport à la ruche
-Nb_zones=8 #nombre de zones florales à découvrir
+Nb_zones=25 #nombre de zones florales à découvrir
 Arrivée=0 #0 signifie que les butineuses partent, 1 qu'elles butinent et 2 qu'elles rentrent à la rûche et 3 qu'elles sont dans la rûche
 Nb_groupes=0 #nombre de groupe de butineuses
 T_groupe=0 #Taille des groupes de butineuses
+qtt_miel=10000000 #qantité de miel dans la rûche en mg
+taux_mort=121 #nombre de secondes avant la mort d'une abeille (naturellement), obtenu en divisant l'espérance de vie d'une abeille par le nombre d'abeilles
 Liste_direct=[] #Liste des directions à prendre pour aller vers une fleur
-Temps_recolte_passe=0 #temps réellement passé par les butineuses sur les fleurs actuellement
+Temps_recolte_passe=0 #temps réeelement passé par les butineuses sur les fleurs actuellement
 for i in Liste_ruches:
     x0,y0=i
-    Info_ruches.append((x0,y0,Nb_abeilles,Nb_ab_dedans,Nb_but_dehors,reperage,liste_reper,distance,Nb_zones,Arrivée,Nb_groupes,T_groupe,Liste_direct,Temps_recolte_passe))
+    Info_ruches.append((x0,y0,Nb_abeilles,Nb_ab_dedans,Nb_but_dehors,reperage,liste_reper,distance,Nb_zones,Arrivée,Nb_groupes,T_groupe,Liste_direct,Temps_recolte_passe,qtt_miel))
 nb_steps=5000 #nombre d'images total
-Nb_zone_fleur= 1200 #nombre de zones florales
+Nb_zone_fleur= 200 #nombre de zones florales
 liste_zone_fleur=[] #liste des coordonnées des zones florales
 while len(liste_zone_fleur)<Nb_zone_fleur:
     x=np.random.choice(N)
@@ -38,10 +40,14 @@ while len(liste_zone_fleur)<Nb_zone_fleur:
     y1 = y % N
     if (x1,y1) not in liste_zone_fleur and (x1,y1) not in Liste_ruches:
         liste_zone_fleur.append((x1,y1))
-taux_nat=217 #nombre de secondes avant une naissance
-s=0 #nombre de secondes écoulées modulo taux_nat
-Temps_interrecolte=7200 #nombre de secondes entre chaque récolte
+taux_nat=57 #nombre de secondes avant une naissance
+s=0 #nombre de secondes écoulées
+j=0 #nombre de jours écoulés
+saison='printemps' #saison de départ
+Temps_interrecolte=3720 #nombre de secondes entre chaque récolte
 Temps_recolte=600 #temps à butiner
+Nb_abeilles_tot=Nb_ruches*Nb_abeilles #nombre total d'abeilles dans la région
+Liste_Nb_abeilles_tot=[Nb_abeilles_tot]
 
 
 
@@ -49,7 +55,7 @@ Temps_recolte=600 #temps à butiner
 terrain=np.zeros([N,N])
 
 for k in Info_ruches:
-    x0,y0,Nb_abeilles,_,_,_,_,_,_,_,_,_,_,_=k
+    x0,y0,Nb_abeilles,_,_,_,_,_,_,_,_,_,_,_,_=k
     terrain[x0][y0]=Nb_abeilles
 
 
@@ -61,19 +67,60 @@ def simulation():
     global Temps_recolte
     global Temps_interrecolte
     global Info_ruches
+    global taux_nat
+    global taux_mort
+    global saison
+    global Nb_ruches
 
     for i in range(nb_steps):
-    
+        Nb_abeilles_tot=0
         s=s+1
+
+        if s==86400: #une journée écoulée
+            j=j+1
+            s=0
+            if j==91: #une saison écoulée
+                Ab_tot=Liste_Nb_abeilles_tot[nb_steps-1]
+                if saison=='printemps':
+                    taux_nat=216
+                    saison='été'
+                    taux_mort=abs(3628800/(AB_tot/Nb_ruches))#esprérance de vie de 42 jours par le nombre d'abeilles moyen dans une  ruche
+                elif saison=='été':
+                    saison='automne'
+                    taux_mort=abs(17280000/(AB_tot/Nb_ruches)) #esprérance de vie de 200 jours par le nombre d'abeilles moyen dans une  ruche
+                elif saison=='automne':
+                    taux_nat=144
+                    saison='hiver'
+                    taux_mort=abs(3628800/(AB_tot/Nb_ruches)) #esprérance de vie de 42 jours par le nombre d'abeilles moyen dans une  ruche
+                elif saison=='hiver':
+                    taux_nat=57
+                    saison='printemps'
+                    taux_mort=abs(3628800/(AB_tot/Nb_ruches)) #esprérance de vie de 42 jours par le nombre d'abeilles moyen dans une  ruche
+                j=0
+                    
+
+        
         Info_ruches_copy=[]
         for k in Info_ruches:
-            x0, y0, Nb_abeilles, Nb_ab_dedans, Nb_but_dehors, reperage, liste_reper, distance, Nb_zones, Arrivée, Nb_groupes, T_groupe, Liste_direct, Temps_recolte_passe=k
+            x0, y0, Nb_abeilles, Nb_ab_dedans, Nb_but_dehors, reperage, liste_reper, distance, Nb_zones, Arrivée, Nb_groupes, T_groupe, Liste_direct, Temps_recolte_passe,qtt_miel=k
 
 
+            if s==60000: #Au bout d'un certain temps, les abeilles vont consommer le miel
+                qtt_miel=qtt_miel-3*Nb_abeilles-14000 # 3mg mangés par chaque abeille, 14g utilisés pour la production de cire
+
+
+            if s/taux_mort==int(s/taux_mort): #mort d'une abeille naturelle
+                Nb_abeilles=Nb_abeilles-1
+                Nb_ab_dedans=Nb_ab_dedans-1
+                terrain[x0][y0]=Nb_ab_dedans
+
+            
             if s/taux_nat==int(s/taux_nat):                 #naissance d'une abeille
                 Nb_abeilles=Nb_abeilles+1
                 Nb_ab_dedans=Nb_ab_dedans+1
                 terrain[x0][y0]=Nb_ab_dedans
+
+                                  
             if reperage<Nb_zones:
                 liste_repera=liste_reper.copy()
                 distance=distance+1
@@ -86,7 +133,7 @@ def simulation():
                 liste_reper=liste_repera.copy()
             if reperage>=Nb_zones:
             
-                if Nb_but_dehors==0 and Arrivée==0:
+                if Nb_but_dehors==0 and Arrivée==0 and s<=43200: #Avant que la moitié de la journée soit écoulée
                     Nb_butineuses=abs(Nb_abeilles/2)
                     Nb_groupes=reperage #nombre de groupe de butineuses
                     T_groupe=abs(Nb_butineuses/Nb_groupes) #nombre d'abeilles dans un groupe
@@ -137,8 +184,17 @@ def simulation():
                             (a,b),(c,d)=k
                             if (a,b)==(c,d):
                                 valid=valid+1
-                        if valid == len(Liste_direct): 
+                        if valid == len(Liste_direct):
                             Arrivée=1
+                            for k in Liste_direct:
+                                (a,b),(c,d)=k
+                                crabe_araignée=np.random.choice(100) #probabilité qu'un crabe araignée se situe dans cette zone et mange une abeille
+                                if crabe_araignée==50:
+                                    terrain[a][b]=terrain[a][b]-1
+                                    Nb_but_dehors=Nb_but_dehors-1
+                                    Nb_abeilles=Nb_abeilles-1
+                                  
+                                  
                     elif Arrivée==1:
                         Temps_recolte_passe=Temps_recolte_passe+1
                         if Temps_recolte_passe==Temps_recolte:
@@ -192,6 +248,9 @@ def simulation():
                         if valid == len(Liste_direct):
                             Arrivée=3
                             Nb_ab_dedans=Nb_ab_dedans+Nb_but_dehors
+                            qtt_miel=qtt_miel+13*Nb_but_dehors #chaque abeille ramène 13 mg de miel
+                            if qtt_miel>30000000: #capacité de miel maximum dans une rûche en mg
+                                qtt_miel=30000000
                             Nb_but_dehors=0
                 elif Arrivée==3 and Nb_but_dehors==0:
                     Temps_recolte_passe=Temps_recolte_passe+1
@@ -200,12 +259,14 @@ def simulation():
                         Arrivée=0
                 
                     
-            Info_ruches_copy.append((x0,y0,Nb_abeilles,Nb_ab_dedans,Nb_but_dehors,reperage,liste_reper,distance,Nb_zones,Arrivée,Nb_groupes,T_groupe,Liste_direct,Temps_recolte_passe))
+            Info_ruches_copy.append((x0,y0,Nb_abeilles,Nb_ab_dedans,Nb_but_dehors,reperage,liste_reper,distance,Nb_zones,Arrivée,Nb_groupes,T_groupe,Liste_direct,Temps_recolte_passe,qtt_miel))
+            Nb_abeilles_tot=Nb_abeilles_tot+Nb_abeilles
+        Liste_Nb_abeilles_tot.append(Nb_abeilles_tot)
         Info_ruches=Info_ruches_copy.copy()
         results.append(terrain.copy())
-    return(results)
+    return(results,Liste_Nb_abeilles_tot)
  
-results=simulation()
+results,_=simulation()
 
     
 fig = plt.figure()
@@ -222,4 +283,3 @@ def updatefig(i):
 ani = anim.FuncAnimation(fig, updatefig, frames=1000, interval=50, blit=True)
 
 plt.show()
-
